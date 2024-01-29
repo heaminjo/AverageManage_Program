ab1f03ccab4a23c528256ad1ff92429b5ae4b93f
heaminjo <157016835+heaminjo@users.noreply.github.com>
2024-01-30 02:01:13 +0900

    first_commit
    학생 수 와 과목 수  Scanner

---
+++ AverageManage_Program.java
@@ -0,0 +1,22 @@
+package first_Projectd;
+
+import java.util.Scanner;
+
+public class AverageManage_Program {
+
+	public static void main(String[] args) {
+		int student[][] = null;
+		
+		int number,subject;
+		Scanner sc = new Scanner(System.in);
+		System.out.println("학생 수:");
+		number = sc.nextInt();
+		
+		System.out.println("과목 수:");
+		subject = sc.nextInt();
+		
+		sc.close();
+		
+	}
+
+}
