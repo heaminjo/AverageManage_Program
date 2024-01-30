535c61d3f70e722dc6996b243ad0c96e69c541cc
heaminjo <157016835+heaminjo@users.noreply.github.com>
2024-01-30 23:46:07 +0900

    AverageManage_Program02.java
    과목 수를 입력 받지않고 클래스에 구현되도록함

--- AverageManage_Program.java
+++ AverageManage_Program.java
@@ -5,16 +5,13 @@ import java.util.Scanner;
 public class AverageManage_Program {
 
 	public static void main(String[] args) {
-		int student[][] = null;
 		
-		int number,subject;
+		int number;
+		
 		Scanner sc = new Scanner(System.in);
 		System.out.println("학생 수:");
 		number = sc.nextInt();
-		
-		System.out.println("과목 수:");
-		subject = sc.nextInt();
-		
+
 		sc.close();
 		
 	}
